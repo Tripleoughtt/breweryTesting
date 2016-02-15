4825ab0b2b89f19a74a90f551a2f94c4f48c77ee
Richard Gautereaux <ReliX@mothership-zeta.attlocal.net>
2016-02-15 12:37:39 -0800

    Changed some redirection

--- public/js/controllers/registerCtrl.js
+++ public/js/controllers/registerCtrl.js
@@ -2,10 +2,10 @@
 
 var app = angular.module('testApp');
 
-app.controller('registerCtrl', function($scope, UserService) {
+app.controller('registerCtrl', function($scope, UserService, $state) {
     $scope.registerUser = (email, password, pass2) => {
       if(password === pass2){
-        UserService.registerUser({email, password});
+        UserService.registerUser({email, password}).success((res) => $state.go('login'));
       } else {
         console.log('unsuccessful pass validation')
       }
